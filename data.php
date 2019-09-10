b12a2f580e87912a3c5045eb7ca43a018871584c
SayanaPM <54029200+SayanaPM@users.noreply.github.com>
2019-09-10 20:20:02 +0530

    Add files via upload

---
+++ data.php
@@ -0,0 +1,14 @@
+<?php
+$name=$_POST['name'];
+$uname=$_POST['uname'];
+$pass=$_POST['pass'];
+$email=$_POST['email'];
+echo "Name:".$name;
+echo "<br>";
+echo "Username:".$uname;
+echo "<br>";
+echo "Password:".$pass;
+echo "<br>";
+echo "Email:".$email;
+echo "<br>";
+?>
---
+++ index.php
@@ -0,0 +1,21 @@
+
+
+
+<html>
+<head><title>Hospital</title></head>
+
+<body>
+
+<form action="data.php" method="POST" align="center">
+<table align="center">
+<tr><td>Name :</td><td><input type="text" name="name" placeholder="First Name"></td></tr>
+<tr><td>User Name :</td><td><input type="text" name="uname" placeholder="User Name" required></td></tr>
+<tr><td>Password :</td><td><input type="Password" name="pass" placeholder="********" required></td></tr>
+<tr><td>Email :</td><td><input type="text" name="email" placeholder="abc@xyz.com" required></td></tr>
+</table><br>
+<input type="submit" value="Submit" name="submit" >
+</form>
+</body>
+</html>
+
+
